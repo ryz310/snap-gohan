54b3b59a9ccb5a67bc42a6b7d95178e399cf71c3
ryosuke_sato <ryz310+git@gmail.com>
2013-09-01 16:31:39 +0900

    メニューと食材のリレーションを設定。

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,6 @@
 class Food < ActiveRecord::Base
   belongs_to :group, class_name: :FoodGroup, foreign_key: :food_group_id
+  
+  has_many :foodstuffs, dependent: :destroy
+  has_many :menus, through: :foodstuffs
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,9 @@
 class Menu < ActiveRecord::Base
   belongs_to :category, class_name: :MenuCategory, foreign_key: :category_id
+  
+  has_many :foodstuffs, dependent: :destroy
+  has_many :foods, through: :foodstuffs
+  
   mount_uploader :image, MenuImageUploader
   validates :name,  presence: true
   validates :image, file_size: {maximum: 1024.kilobytes.to_i}
