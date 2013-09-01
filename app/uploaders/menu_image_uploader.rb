65a76de53279c681c3817d21b6a9eb0170f0b329
ryosuke_sato <ryz310+git@gmail.com>
2013-09-01 22:19:10 +0900

    不要なコメントを削除

--- app/uploaders/menu_image_uploader.rb
+++ app/uploaders/menu_image_uploader.rb
@@ -1,11 +1,8 @@
 # encoding: utf-8
 
 class MenuImageUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
+  
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -26,10 +23,6 @@ class MenuImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   process resize_to_fit: [400, 400]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
 
   # Create different versions of your uploaded files:
   version :thumb do
