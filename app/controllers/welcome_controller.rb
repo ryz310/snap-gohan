5c049a98fa46048952bfb7d4663eaf773c17b90f
ryosuke_sato <ryz310+git@gmail.com>
2013-09-04 02:02:35 +0900

    トップページのデザインを調整。

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,17 +3,24 @@ class WelcomeController < ApplicationController
   PICTURE_COUNT = 10
   
   def index
-    seed = [0, Menu.count - PICTURE_COUNT - 1].max
-    
-    ids = Menu
-            .offset(rand seed)
-            .limit(500)
-            .pluck(:id)
-            .sample(PICTURE_COUNT)
-            
-    @pictures = Menu
-                  .where(id: ids)
-                  .where("image is not null")
-                  .select(:id, :image)
+      seed = [0, Menu.count - PICTURE_COUNT - 1].max
+      
+      ids = Menu
+              .offset(rand seed)
+              .limit(500)
+              .pluck(:id)
+              .sample(PICTURE_COUNT)
+              
+      @pictures = Menu
+                    .where(id: ids)
+                    .where("image is not null")
+                    .select(:id, :image)
+                    .map { |p|
+                      p.image.url(:thumb).to_s
+                    }
   end
+  
+  private
+    def sample_pictures
+    end
 end
