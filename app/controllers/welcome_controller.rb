c373c37fe6b6944ad20e94281f13d5ead9308b21
ryosuke_sato <ryz310@gmail.com>
2013-09-03 10:24:42 +0900

    トップページのデザインを少し作成。 メニュー写真のcssを修正

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,19 @@
 class WelcomeController < ApplicationController
+  
+  PICTURE_COUNT = 10
+  
   def index
+    seed = [0, Menu.count - PICTURE_COUNT - 1].max
+    
+    ids = Menu
+            .offset(rand seed)
+            .limit(500)
+            .pluck(:id)
+            .sample(PICTURE_COUNT)
+            
+    @pictures = Menu
+                  .where(id: ids)
+                  .where("image is not null")
+                  .select(:id, :image)
   end
 end
