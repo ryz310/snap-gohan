7994515d0256eab04188acb4cee1500d83c5c6d3
ryosuke_sato <ryz310@gmail.com>
2013-09-04 11:59:55 +0900

    DB定義変更に合わせてフォームを修正。

--- db/migrate/20130831145710_create_menu_categories.rb
+++ db/migrate/20130831145710_create_menu_categories.rb
@@ -2,7 +2,7 @@ class CreateMenuCategories < ActiveRecord::Migration
   def change
     create_table :menu_categories do |t|
       t.string :name,        null: false, limit: 64, default: ""
-      t.text   :description
+      t.text   :description, limit: 1024
 
       t.timestamps
     end
