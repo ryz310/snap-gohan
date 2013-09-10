a8f5b3faf8e7cd1d5e235956ee1a8807bf147ab9
ryosuke_sato <ryz310+git@gmail.com>
2013-09-10 21:51:41 +0900

    JavaScriopt を CoffeeScript に変換。

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,39 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-function remove_fields(link) {
-  $(link).prev("input[type=hidden]").val("1");
-  $(link).closest(".fields").hide();
-}
-
-function add_fields(link, association, content, target) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g");
-  $(target).append(content.replace(regexp, new_id));
-}
-
-function update_food_select(link, group_id, server) {
-  $.ajax({
-    url:  server,
-    type: "GET",
-    data: {"group_id" : group_id},
-    dataType: "html",
-    success: function(ajax) {
-      $(link).next("select").empty();
-      $(link).next("select").html(ajax);
-    }
-  });
-}
--- app/assets/javascripts/menus.js.coffee
+++ app/assets/javascripts/menus.js.coffee
@@ -1,3 +1,22 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+# 
+@remove_fields = (link) ->
+  $(link).prev("input[type=hidden]").val "1"
+  $(link).closest(".fields").hide()
+  
+#
+@add_fields = (link, association, content, target) ->
+  new_id = new Date().getTime()
+  regexp = new RegExp("new_" + association, "g")
+  $(target).append content.replace(regexp, new_id)
+
+#   
+@update_food_select = (link, group_id, server) ->
+  $.ajax
+    url:  server
+    type: "GET"
+    data:
+      group_id: group_id
+    dataType: "html"
+    success: (ajax) ->
+      $(link).next("select").empty()
+      $(link).next("select").html ajax
