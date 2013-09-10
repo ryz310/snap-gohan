517078557759f77fad4ba10d02e611a08e80d818
ryosuke_sato <ryz310+git@gmail.com>
2013-09-10 21:53:19 +0900

    link_to_function が rails4.1 より廃止されるので link_to に置換。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+    f.hidden_field(:_destroy) + link_to(name, "#", onclick: "remove_fields(this); return false;")
   end
   
   def link_to_add_fields(name, f, association, target)
@@ -8,6 +8,6 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
-    link_to_function(name, %Q[add_fields(this, "#{association}", "#{escape_javascript(fields)}", "#{target}")])
+    link_to name, "#", onclick: %Q[add_fields(this, "#{association}", "#{escape_javascript(fields)}", "#{target}"); return false;]
   end
 end
