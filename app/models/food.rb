14fa50720e34c33abc141b13bed4f95b79a96d55
ryosuke_sato <ryz310@gmail.com>
2013-11-21 22:56:56 +0900

    models/food において、現在有効な栄養素のみ select するように default_scope を設定。

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,19 +5,39 @@ class Food < ActiveRecord::Base
   has_many :menus, through: :foodstuffs
   
   # number "00000" ~ "01000" は管理用データ
-  default_scope where.not(number: "00000".."01000")
+  # 管理用データで有効となっている栄養素名一覧を取得
+  default_scope -> { where.not(number: "00000".."01000")
+                    .select(:id, *Food.enable_columns) }
 
-  #
+  # 栄養素名
+  NUTRIENTS_ALL = [:energy, :energy_kj, :water, :protein, :protein_sum_of_amino_acid_residuse, :lipid, :triacylglycerol_equivalents, :carbohydrate, :ash, :sodium, :potassium, :calcium, :magnesium, :phosphorus, :iron, :zinc, :copper, :manganese, :iodine, :selenium, :chromium, :molybdenum, :retinol, :alpha_carotenes, :beta_carotenes, :beta_cryptoxanthin, :bata_carotene_equivalents, :retinol_activity_equivalents, :vitamind, :alpha_tocopherols, :beta_tocopherols, :gamma_tocopherols, :delta_tocopherols, :vitamink, :thiamin, :riboflavin, :niacin, :vitaminb6, :vitaminb12, :folate, :pantothenic_acid, :biotin, :ascorbic_acid, :saturated_fatty_acids, :monounsaturated_fatty_acids, :polyunsaturated_fatty_acids, :cholesterol, :soluble_dietary_fibers, :insoluble_dietary_fibers, :total_dietary_fibers, :salt_equivalents].freeze
+
+  # 管理用データを取得
+  def Food.setting
+    Food.unscoped
+      .where(number: "00000")
+      .first
+  end
+
+  # 管理用データで有効となっている栄養素名一覧を取得
+  def Food.nutrients
+    NUTRIENTS_ALL.select do |nutrient|
+      Food.setting.public_send(nutrient) != 0.0
+    end
+  end
+
+  # 管理用データで有効となっている栄養素名一覧を取得
+  def Food.enable_columns
+    [:food_group_id, :number, :classification, :subdivision, :name, :refuse, *Food.nutrients]
+  end
+
+  # 食品の「大分類」と「中分類/小分類/細分」をまとめて取得
   def full_name
     "#{self.classification} : #{self.subdivision}"  
   end
   
-  #
+  # 廃棄率をパーセント表記
   def refuse_percentage
     (self.refuse * 100).round
-  end
-
-  def Food.setting
-    Food.unscoped.where(number: "00000").first
   end
 end
